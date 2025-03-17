2867b08907df0943195def2587ad4fde64544aa1
Lrelni <132231253+Lrelni@users.noreply.github.com>
2025-03-17 17:43:00 -0600

    Backup commit

--- serpent/serpent_gui.py
+++ serpent/serpent_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import font
 from abc import ABC, abstractmethod
 
 import pyaudio 
@@ -15,10 +16,24 @@ class BackingTrack(ttk.Frame):
 
         self.osc = srpt_audio.SineOscillator()
         self.player = srpt_audio.Player(self.osc, settings.rate, settings.frames_per_buffer)
+        self._time_signature = (3,4)
 
-        self.button = ttk.Button(self, command=self.button_callback, text="Testing Button")
-        self.button.pack()
-
+        self.time_signature_font = font.Font(family="Helvetica", size=48, weight="bold")
+        self.time_signature_labels = [ttk.Label(self, text=str(self.time_signature[0]), font=self.time_signature_font), 
+                                      ttk.Label(self, text=str(self.time_signature[1]), font=self.time_signature_font)]
+        for label in self.time_signature_labels:
+            label.pack()
+    
+    @property
+    def time_signature(self):
+        return self._time_signature
+    
+    @time_signature.setter
+    def time_signature(self, val):
+        self.time_signature = val
+        self.time_signature_labels[0].config(text=str(self.time_signature[0]))
+        self.time_signature_labels[1].config(text=str(self.time_signature[1]))
+        
     def button_callback(self):
         self.osc.amp = 1 - self.osc.amp
 
