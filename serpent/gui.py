51eb7928ef626d8b49531caf2cdc11af4da2b069
Lrelni <132231253+Lrelni@users.noreply.github.com>
2025-04-09 18:36:10 -0600

    Add About box, rename Serpent class to MainFrame

--- serpent/gui.py
+++ serpent/gui.py
@@ -6,16 +6,23 @@ import audio
 import settings
 
 
-class Serpent(wx.Frame):
+class AboutBox(wx.Dialog):
     def __init__(self, *args, **kw):
-        super().__init__()
+        super().__init__(*args, **kw)
+        self.panel = wx.Panel(self)
+        text = wx.StaticText(self.panel, label="Serpent v" + settings.version)
+
+
+class MainFrame(wx.Frame):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
 
         self.SetSize(1280, 900)
         self.panel = wx.Panel(self)
         self.panel.Sizer = wx.BoxSizer(wx.VERTICAL)
 
         self.notebook = wx.Notebook(self.panel)
-        self.panel.Sizer.Add(self.notebook)
+        self.panel.Sizer.Add(self.notebook, proportion=1, flag=wx.EXPAND)
 
         self.help = wx.Menu()
         self.about = self.help.Append(
@@ -25,5 +32,15 @@ class Serpent(wx.Frame):
         self.menubar = wx.MenuBar()
         self.menubar.Append(self.help, "&Help")
         self.SetMenuBar(self.menubar)
+        self.Bind(wx.EVT_MENU, self.create_about, self.about)
 
         self.Show(True)
+
+    def create_about(self, event):
+        about_box = AboutBox(self, title="About")
+        about_box.Show()
+
+    def add_modules(self, modules: list[typing.Type[wx.Panel]]):
+        for module in modules:
+            instance = module(parent=self.notebook)
+            self.notebook.AddPage(instance, instance.title)
